6c2cdf21ce20291ec3b60f73c44076611af66ebd
Fateh Singh <fateh288@gmail.com>
2022-04-19 05:10:21 +0000

    deadlocking/packet loss issue fixed with dgl version 0.7.0 + other minor changes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # distributed_graph_learning_ethereum_dataset
 
+USE DGL version 0.7.0 to avoid deadlocking/packet loss issue. Issue exists in v0.8 
+
 1) Run pull_data.py from dataset folder --- fetches data from big query and saves to csv file
 2) Run create_graphs.py from dataset folder --- creates dgl graph with required metadata from csv file
 3) Run preprocessing.py from distributed_training folder --- partitions the dgl graph for distributed training
--- distributed_training/launch_script.sh
+++ distributed_training/launch_script.sh
@@ -4,6 +4,6 @@
 --num_samplers 1 \
 --num_servers 1 \
 --num_omp_threads 1 \
---part_config /home/diml_2022/workspace_4/partitioned_graphs_100M/partitioned_graph_100M.json \
+--part_config /home/diml_2022/workspace_4/partitioned_graphs_1M/partitioned_graph_1M.json \
 --ip_config /home/diml_2022/workspace_4/ip_config.txt \
-"/home/diml_2022/anaconda3/envs/diml_2022/bin/python training_scripts/train_dist.py --graph_name 'partitioned_graph_100M' --ip_config ~/workspace_4/ip_config.txt --part_config ~/workspace_4/partitioned_graphs_100M/partitioned_graph_100M.json --batch_size_eval 1000 --num_epochs 2 --eval_every 2"
+"/home/diml_2022/anaconda3/envs/diml_2022/bin/python training_scripts/train_dist.py --graph_name 'partitioned_graph_1M' --ip_config ~/workspace_4/ip_config.txt --part_config ~/workspace_4/partitioned_graphs_1M/partitioned_graph_1M.json --batch_size_eval 1000 --num_epochs 2 --eval_every 2"
